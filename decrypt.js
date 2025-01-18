fa49786ebeab2a3c188a4d921197d83ad3260062
ThierryLeroux <122485800+ThierryLeroux@users.noreply.github.com>
2025-01-17 21:00:50 -0500

    Update decrypt.js

--- decrypt.js
+++ decrypt.js
@@ -9,11 +9,19 @@ function decryptText(encryptedText) {
     return decryptedText;
 }
 
-// Read the encrypted data from a file
-const encryptedData = fs.readFileSync('encrypted_data.txt', 'utf8');
+// Check if the encrypted data file exists
+const filePath = 'encrypted_data.txt';
+if (fs.existsSync(filePath)) {
+    // Read the encrypted data from the file
+    const encryptedData = fs.readFileSync(filePath, 'utf8');
 
-// Decrypt the data
-const decryptedData = decryptText(encryptedData);
+    // Decrypt the data
+    const decryptedData = decryptText(encryptedData);
 
-// Write the decrypted data to a file
-fs.writeFileSync('decrypted_data.txt', decryptedData);
+    // Write the decrypted data to a file
+    fs.writeFileSync('decrypted_data.txt', decryptedData);
+    console.log('Decryption successful. Decrypted data written to decrypted_data.txt');
+} else {
+    console.error(`Error: File not found - ${filePath}`);
+    process.exit(1);
+}
